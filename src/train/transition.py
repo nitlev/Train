558bdf4014d3457c40bc870c2aff070a72e5eb6e
Veltin Dupont <vdupont@octo.com>
2016-10-16 20:59:05 +0200

    Renaming reward_function attribute

--- src/train/transition.py
+++ src/train/transition.py
@@ -2,12 +2,12 @@ class Transition:
     def __init__(self, previous_state, next_state, reward_function):
         self.previous_state = previous_state
         self.next_state = next_state
-        self.rewad_function = reward_function
+        self.reward_function = reward_function
         self._reward = None
 
     @property
     def reward(self):
         if self._reward is None:
-            self._reward = self.rewad_function(self.previous_state,
-                                               self.next_state)
+            self._reward = self.reward_function(self.previous_state,
+                                                self.next_state)
         return self._reward
