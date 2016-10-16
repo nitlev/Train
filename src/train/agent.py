5497c8834c5b3827ca12b26008ebf3d1b0cc2010
Veltin Dupont <vdupont@octo.com>
2016-10-16 21:51:50 +0200

    Creating state_to_actions_attribute

--- src/train/agent.py
+++ src/train/agent.py
@@ -1,10 +1,16 @@
 from src.train.action import Actions
 
 
+def state_to_empty_actions(state):
+    return Actions()
+
+
 class Agent:
-    def __init__(self, state=None, update_function=lambda s, a: s):
+    def __init__(self, state=None, update_function=lambda s, a: s,
+                 state_to_action_function=state_to_empty_actions):
         self.state = state
         self.update_function = update_function
+        self.state_to_actions_function = state_to_action_function
 
     def choose_best_action(self, actions):
         return actions[0]
@@ -13,4 +19,4 @@ class Agent:
         self.state = self.update_function(self.state, action)
 
     def possible_actions(self):
-        return Actions()
+        return self.state_to_actions_function(self.state)
